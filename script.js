571bab704cf4109a01ced59beeeabc835fb0e82d
mcmckenne <73710953+mcmckenne@users.noreply.github.com>
2020-12-04 20:20:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//makes img bigger with event:
+
+function bigImg(x) {
+  x.style.height = "500px";
+  x.style.width = "380px";
+}
+
+function normalImg(x) {
+  x.style.height = "320px";
+  x.style.width = "180px";
+}
+
+// onclick input
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Welcome to My About Me Page!";
+}
+// change css
+
+function myFunction() {
+  document.getElementById("demo").style.textAlign = "center";
+}
+
+// window alert
+
+function myFunction() {
+  alert("Hope You Enjoyed Your Visit!");
+}
